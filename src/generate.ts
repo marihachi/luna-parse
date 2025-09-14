a6559651809dbd9248783a7ebaae883c683d1253
marihachi <marihachi0620@gmail.com>
2025-09-14 16:11:17 +0900

    add script

---
+++ src/generate.ts
@@ -0,0 +1,16 @@
+import fs from "node:fs";
+import path from "node:path";
+import { generateCode } from "./spec/index.js";
+
+// load
+const resolvedPath = path.resolve("./debug/debug.spec");
+let fileContent;
+try {
+    fileContent = fs.readFileSync(resolvedPath, { encoding: 'utf8' });
+} catch (err) {
+    throw new Error('Failed to load a spec file.');
+}
+
+const code = generateCode(fileContent);
+
+console.log(code);
